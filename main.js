b7f126a11f75cbf07586162a1006ebdb9aea7db6
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-16 17:01:21 -0500

    BUG: Function in form tag runs on refresh

--- main.js
+++ main.js
@@ -5,32 +5,54 @@ let checkedNum = []
 
 
 const generateTeam = async () => {
+    // console.log(document.getElementById('picture')) == null
     
+    if(document.getElementById('picture') != null) {
+        while(document.getElementById('picture') != null){
+            var image_x = document.getElementById('picture');
+            image_x.parentNode.removeChild(image_x);
+        }
+    }
+
+    
+
+    
+
     try {
         validation = []
+        pokemonTeam = []
+        checkedNum = []
+
+        // const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        // const pokemon = response.data.results[2]
+        // const response2 = await axios.get(pokemon.url)
+        // let type = extractTypes(pokemon, response2);
+        // pokemonTeam.push(pokemon)
+        // console.log(pokemon)
+
         while(pokemonTeam.length < 6){
             getRandomNum()
             
-            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-            const pokemon = response.data.results[randID]
-            const response2 = await axios.get(pokemon.url)
+            const response3 = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+            const pokemon2 = response3.data.results[randID]
+            const response4 = await axios.get(pokemon2.url)
             
-            let type = extractTypes(pokemon, response2);
+            let type = extractTypes(pokemon2, response4);
 
-            if(checkTypes(validation, type)) {
-                pokemonTeam.push(pokemon)
+            if(checkTypes(type)) {
+                pokemonTeam.push(pokemon2)
             }
 
-            console.log(pokemonTeam)
-        
         }
-
+        console.log(pokemonTeam)
         displayPictures(pokemonTeam);
         
 
     } catch(e) {
         console.log('Error:', e)
     }
+
+    
 
 }
 
@@ -41,12 +63,17 @@ function getRandomNum() {
     return randID
 }
 
-function checkTypes(arr, type) {
-    if(arr.length < 1) {
-        arr.push(type)
+function checkTypes(type) {
+    if(validation.length === 0) {
+        validation = type.slice(0)
+        console.log("validation", validation)
+        console.log("validation.length",validation.length)
     } else {
         for(element of type) {
-            if(arr[0].includes(element)) {
+            console.log('validation[0]', validation[0])
+            console.log('element', element)
+            if(validation[0].includes(element)) {
+                console.log("Success")
                 return true;
             }
         }
@@ -54,9 +81,9 @@ function checkTypes(arr, type) {
 
     return false;
 }
-let urls = []
+
 const displayPictures = async (team) => {
-    
+    let urls = []
     for(pokemon of team) {
 
         const response = await axios.get(pokemon.url)
@@ -67,6 +94,7 @@ const displayPictures = async (team) => {
     for(url of urls){
         let img = document.createElement('img')
         img.src=url
+        img.id=('picture')
         document.body.appendChild(img)
     }
 
